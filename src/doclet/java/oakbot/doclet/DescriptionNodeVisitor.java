0e8576776cafd3ce027803afabd8b10b1309e43f
Mike Angstadt <mike.angstadt@gmail.com>
2014-12-16 21:45:11 -0500

    Moved "DescriptionNodeVisitor" to the doclet package.

--- src/doclet/java/oakbot/doclet/DescriptionNodeVisitor.java
+++ src/doclet/java/oakbot/doclet/DescriptionNodeVisitor.java
@@ -1,4 +1,4 @@
-package oakbot.command.javadoc;
+package oakbot.doclet;
 
 import java.util.ArrayList;
 import java.util.Iterator;
--- src/doclet/java/oakbot/doclet/OakbotDoclet.java
+++ src/doclet/java/oakbot/doclet/OakbotDoclet.java
@@ -22,8 +22,6 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import oakbot.command.javadoc.DescriptionNodeVisitor;
-
 import org.jsoup.Jsoup;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
