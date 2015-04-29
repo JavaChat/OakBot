fdc4d0a7cd769797937c2d6946dca8b784f883be
Mike Angstadt <mike.angstadt@gmail.com>
2015-04-29 10:15:55 -0400

    Check to see if file exists first.

--- src/main/java/oakbot/Statistics.java
+++ src/main/java/oakbot/Statistics.java
@@ -1,32 +1,38 @@
 package oakbot;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import oakbot.util.PropertiesWrapper;
 
-public class Statistics extends PropertiesWrapper {
+/**
+ * Records statistics on the bot.
+ * @author Michael Angstadt
+ */
+public class Statistics {
 	private static final Logger logger = Logger.getLogger(Statistics.class.getName());
+	private final PropertiesWrapper properties;
 	private final Path file;
 	private int responses;
 
 	public Statistics(Path file) throws IOException {
-		super(file);
+		this.properties = Files.exists(file) ? new PropertiesWrapper(file) : new PropertiesWrapper();
 		this.file = file;
-		responses = getInteger("responses", 0);
+		responses = properties.getInteger("responses", 0);
 	}
 
 	public synchronized void incMessagesRespondedTo() {
 		responses++;
-		set("responses", responses);
+		properties.set("responses", responses);
 		save();
 	}
 
 	private void save() {
 		try {
-			store(file);
+			properties.store(file);
 		} catch (IOException e) {
 			logger.log(Level.WARNING, "Could not persist statistics data.", e);
 		}
