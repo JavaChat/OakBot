c585c0adc496b68004b49c62e2e963c81b23515d
Michael Angstadt <mike.angstadt@gmail.com>
2015-08-29 12:03:06 -0400

    Added ability to specify choices for /roll.

--- src/main/java/oakbot/command/RollCommand.java
+++ src/main/java/oakbot/command/RollCommand.java
@@ -7,7 +7,7 @@ import oakbot.chat.ChatMessage;
 import oakbot.util.ChatBuilder;
 
 /**
- * Rolls a variable-sided die.
+ * Rolls a variable-sided die or makes a choice.
  * @author Michael Angstadt
  */
 public class RollCommand implements Command {
@@ -20,29 +20,70 @@ public class RollCommand implements Command {
 
 	@Override
 	public String description() {
-		return "Rolls a variable-sided die.";
+		return "Rolls a variable-sided die or makes a choice.";
 	}
 
 	@Override
 	public String helpText(String trigger) {
 		//@formatter:off
 		return new ChatBuilder()
-			.append("Rolls a variable-sided die.").nl()
+			.append("Rolls a variable-sided die or makes a choice.").nl()
 			.append("Usage: ").append(trigger).append(name()).append(" [SIDES=6] [TIMES=1]").nl()
+			.append("Usage: ").append(trigger).append(name()).append(" choice1 choice2 ... choiceN").nl()
 			.append("Example: ").append(trigger).append(name()).nl()
 			.append("Example: ").append(trigger).append(name()).append(" 20").nl()
-			.append("Example: ").append(trigger).append(name()).append(" 20 5")
+			.append("Example: ").append(trigger).append(name()).append(" 20 5").nl()
+			.append("Example: ").append(trigger).append(name()).append(" java scala c#")
 		.toString();
 		//@formatter:on
 	}
 
 	@Override
 	public ChatResponse onMessage(ChatMessage message, boolean isAdmin) {
+		ChatResponse response = choiceRoll(message);
+		if (response != null) {
+			return response;
+		}
+
+		return diceRoll(message);
+	}
+
+	private ChatResponse choiceRoll(ChatMessage message) {
+		String content = message.getContent().trim();
+		if (content.isEmpty()) {
+			//no words specified, do a dice roll
+			return null;
+		}
+
+		String[] words = getParameters(content);
+		if (isNumber(words[0]) && (words.length < 2 || isNumber(words[1]))) {
+			//first two words are numbers, which means it's a dice roll
+			return null;
+		}
+
+		int index = random.nextInt(words.length);
+		String word = words[index];
+
+		//@formatter:off
+		return new ChatResponse(new ChatBuilder()
+			.reply(message)
+			.append(word)
+		);
+		//@formatter:on
+
+	}
+
+	private boolean isNumber(String value) {
+		return value.matches("\\d+");
+	}
+
+	private ChatResponse diceRoll(ChatMessage message) {
 		int sides = 6;
 		int times = 1;
 		String content = message.getContent().trim();
 		if (!content.isEmpty()) {
-			String split[] = content.split("\\s+");
+			String split[] = getParameters(content);
+
 			if (split.length > 0) {
 				try {
 					sides = Integer.parseInt(split[0]);
@@ -77,5 +118,9 @@ public class RollCommand implements Command {
 		}
 
 		return new ChatResponse(cb);
+	}
+
+	private String[] getParameters(String content) {
+		return content.split("\\s+");
 	}
 }
