eccc1d15070fbdf51a9299a7b9dc88853aae4f77
Mike Angstadt <mike.angstadt@gmail.com>
2017-03-28 09:28:13 -0400

    Refactor MentionListener to use ChatMessage.getMentions()

--- src/main/java/oakbot/listener/MentionListener.java
+++ src/main/java/oakbot/listener/MentionListener.java
@@ -9,12 +9,10 @@ import oakbot.util.ChatBuilder;
  * @author Michael Angstadt
  */
 public class MentionListener implements Listener {
-	private final String mention, mentionWithoutSpaces;
-	private final String trigger;
+	private final String botUsernameMention, trigger;
 
-	public MentionListener(String botName, String trigger) {
-		mention = "@" + botName.toLowerCase();
-		mentionWithoutSpaces = mention.replace(" ", "");
+	public MentionListener(String botUsername, String trigger) {
+		botUsernameMention = botUsername.replace(" ", "").toLowerCase();
 		this.trigger = trigger;
 	}
 
@@ -35,17 +33,30 @@ public class MentionListener implements Listener {
 
 	@Override
 	public ChatResponse onMessage(ChatMessage message, boolean isAdmin) {
-		String contentToLower = message.getContent().toLowerCase();
-		if (!contentToLower.contains(mention) && !contentToLower.contains(mentionWithoutSpaces)) {
-			return null;
+		if (mentioned(message)) {
+			//@formatter:off
+			return new ChatResponse(new ChatBuilder()
+				.reply(message)
+				.append("Type ").code().append(trigger).append("help").code().append(" to see all my commands.")
+			);
+			//@formatter:on
 		}
 
-		//@formatter:off
-		return new ChatResponse(new ChatBuilder()
-			.reply(message)
-			.append("Type ").code().append(trigger).append("help").code().append(" to see all my commands.")
-		);
-		//@formatter:on
+		return null;
 	}
 
+	/**
+	 * Determines if the bot was mentioned in a chat message.
+	 * @param message the chat message
+	 * @return true if the bot was mentioned, false if not
+	 */
+	private boolean mentioned(ChatMessage message) {
+		for (String mention : message.getMentions()) {
+			mention = mention.toLowerCase();
+			if (botUsernameMention.startsWith(mention)) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
---
+++ src/test/java/oakbot/listener/MentionListenerTest.java
@@ -0,0 +1,33 @@
+package oakbot.listener;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import oakbot.bot.ChatResponse;
+import oakbot.chat.ChatMessage;
+
+/**
+ * @author Michael Angstadt
+ */
+public class MentionListenerTest {
+	@Test
+	public void mentioned() {
+		assertMentioned("Hey @OakBot", true);
+		assertMentioned("Hey @Oak", true);
+		assertMentioned("Hey @OakBott", false);
+		assertMentioned("Hey", false);
+	}
+
+	private static void assertMentioned(String message, boolean mentioned) {
+		//@formatter:off
+		ChatMessage chatMessage = new ChatMessage.Builder()
+			.content(message)
+		.build();
+		//@formatter:on
+
+		MentionListener listener = new MentionListener("OakBot", "/");
+		ChatResponse response = listener.onMessage(chatMessage, false);
+		assertEquals(mentioned, response != null);
+	}
+}
