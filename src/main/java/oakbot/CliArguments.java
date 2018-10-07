c444e477d33ceec9a22c95f9aa3149c8dff2b76a
Mike Angstadt <mike.angstadt@gmail.com>
2018-10-07 13:20:04 -0400

    Add CLI "mock" parameter to help message

--- src/main/java/oakbot/CliArguments.java
+++ src/main/java/oakbot/CliArguments.java
@@ -73,6 +73,19 @@ public class CliArguments {
 		"  The path to a JSON file for storing all persistant data." + nl +
 		"  (defaults to \"" + defaultDb + "\")." + nl +
 		nl +
+		"--mock" + nl +
+		"  Runs the bot using a mock chat connection for testing purposes." + nl +
+		"  A text file will be created in the root of the project for each chat room the" + nl +
+		"  bot is configured to connect to. These files are used to \"send\" messages" + nl +
+		"  to the mock chat rooms. To send a message, type your message into the text" + nl +
+		"  file and save it." + nl +
+		"  Messages are entered one per line. Multi-line messages can be entered by" + nl +
+		"  ending each line with a backslash until you reach the last line. You should" + nl +
+		"  only append onto the end of the file; do not delete anything. These files are" + nl +
+		"  re-created every time the program runs." + nl +
+		"  All messages that are sent to the mock chat room are displayed in stdout (this" + nl +
+		"  includes your messages and the bot's responses)." + nl +
+		nl +
 		"--quiet" + nl +
 		"  If specified, the bot will not output a greeting message when it starts up." + nl +
 		nl +
