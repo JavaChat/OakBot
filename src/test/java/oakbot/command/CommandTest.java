a067e91bd28a01f8761cad2d6aab8a40fa287b79
Mike Angstadt <mike.angstadt@gmail.com>
2023-07-12 19:10:15 -0400

    Create unit test for Command

---
+++ src/test/java/oakbot/command/CommandTest.java
@@ -0,0 +1,61 @@
+package oakbot.command;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import oakbot.bot.ChatActions;
+import oakbot.bot.ChatCommand;
+import oakbot.bot.IBot;
+import oakbot.chat.ChatMessage;
+
+/**
+ * @author Michael Angstadt
+ */
+public class CommandTest {
+	@Test
+	public void isInvokingMe() {
+		Command command = new Command() {
+			@Override
+			public String name() {
+				return "name";
+			}
+
+			@Override
+			public List<String> aliases() {
+				return Arrays.asList("one", "two");
+			}
+
+			@Override
+			public HelpDoc help() {
+				return null;
+			}
+
+			@Override
+			public ChatActions onMessage(ChatCommand chatCommand, IBot bot) {
+				return null;
+			}
+		};
+
+		assertIsInvokingMe(command, "/name");
+		assertIsInvokingMe(command, "/name foo");
+		assertIsInvokingMe(command, "/one");
+		assertIsInvokingMe(command, "/one foo");
+		assertNotInvokingMe(command, "/three");
+		assertNotInvokingMe(command, "blah");
+	}
+
+	private static void assertIsInvokingMe(Command command, String content) {
+		ChatMessage message = new ChatMessage.Builder().content(content).build();
+		assertTrue(command.isInvokingMe(message, "/"));
+	}
+
+	private static void assertNotInvokingMe(Command command, String content) {
+		ChatMessage message = new ChatMessage.Builder().content(content).build();
+		assertFalse(command.isInvokingMe(message, "/"));
+	}
+}
