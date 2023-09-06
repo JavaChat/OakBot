bca96e1eea14c1be1af6d8cd5d4679d2b6f7f6a1
Mike Angstadt <mike.angstadt@gmail.com>
2023-09-06 19:31:06 -0400

    Create Botler listener

---
+++ src/main/java/oakbot/listener/BotlerListener.java
@@ -0,0 +1,99 @@
+package oakbot.listener;
+
+import static oakbot.bot.ChatActions.doNothing;
+import static oakbot.bot.ChatActions.post;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.mangstadt.sochat4j.ChatMessage;
+
+import oakbot.bot.ChatActions;
+import oakbot.bot.ChatCommand;
+import oakbot.bot.IBot;
+import oakbot.command.Command;
+import oakbot.command.HelpDoc;
+import oakbot.util.Now;
+
+/**
+ * Allows users to make the bot send commands to Botler, a bot by Captain
+ * Obvious. Also, responds to Botler's startup message.
+ * @author Michael Angstadt
+ * @see "https://stackoverflow.com/users/13750349/botler"
+ * @see "https://github.com/butler1233/stackoverflow-chatbot"
+ */
+public class BotlerListener implements Command, Listener {
+	private final String botlerTrigger;
+	private final int botlerUserId;
+	private final String botlerStartupMessage;
+	private final String response;
+	private final Duration timeBetweenResponses;
+	private final Map<Integer, Instant> responseTimesPerRoom = new HashMap<>();
+
+	/**
+	 * @param botlerTrigger Botler's trigger
+	 * @param botlerUserId Botler's user ID
+	 * @param botlerStartupMessage the Botler message to respond to
+	 * @param response the response
+	 */
+	public BotlerListener(String botlerTrigger, int botlerUserId, String botlerStartupMessage, String response, String timeBetweenResponses) {
+		this.botlerTrigger = botlerTrigger;
+		this.botlerUserId = botlerUserId;
+		this.botlerStartupMessage = botlerStartupMessage;
+		this.response = response;
+		this.timeBetweenResponses = Duration.parse(timeBetweenResponses);
+	}
+
+	@Override
+	public String name() {
+		return "botler";
+	}
+
+	@Override
+	public HelpDoc help() {
+		//@formatter:off
+		return new HelpDoc.Builder((Command)this)
+			.summary("Makes Oak send commands to Botler, a bot by Captain Obvious (be nice).")
+			.detail("Also, responds to Botler's startup message. Botler's GitHub: https://github.com/butler1233/stackoverflow-chatbot")
+			.example("help", "Sends the help command to Botler.")
+		.build();
+		//@formatter:on
+	}
+
+	@Override
+	public ChatActions onMessage(ChatCommand chatCommand, IBot bot) {
+		String content = chatCommand.getContentMarkdown();
+		if (content.isEmpty()) {
+			return doNothing();
+		}
+
+		return post(botlerTrigger + " " + content);
+	}
+
+	@Override
+	public ChatActions onMessage(ChatMessage message, IBot bot) {
+		if (message.getUserId() != botlerUserId) {
+			return doNothing();
+		}
+
+		if (!botlerStartupMessage.equals(message.getContent().getContent())) {
+			return doNothing();
+		}
+
+		Instant now = Now.instant();
+		int roomId = message.getRoomId();
+		Instant lastResponse = responseTimesPerRoom.get(roomId);
+
+		if (lastResponse != null) {
+			Duration sinceLastResponse = Duration.between(lastResponse, now);
+			if (sinceLastResponse.compareTo(timeBetweenResponses) < 0) {
+				return doNothing();
+			}
+		}
+
+		responseTimesPerRoom.put(roomId, now);
+		return post(response);
+	}
+}
