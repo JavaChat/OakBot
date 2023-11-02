8d9a2db5ad416e27bcade998d4f2c94ad37e917d
Mike Angstadt <mike.angstadt@gmail.com>
2023-11-02 10:00:05 -0400

    Add /coffee command

---
+++ src/main/java/oakbot/command/CoffeeCommand.java
@@ -0,0 +1,75 @@
+package oakbot.command;
+
+import static oakbot.bot.ChatActions.error;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.mangstadt.sochat4j.util.Http;
+
+import oakbot.bot.ChatActions;
+import oakbot.bot.ChatCommand;
+import oakbot.bot.IBot;
+import oakbot.bot.PostMessage;
+import oakbot.util.HttpFactory;
+
+/**
+ * Displays a coffee-related picture.
+ * @author Michael Angstadt
+ * @see "https://coffee.alexflipnote.dev"
+ */
+public class CoffeeCommand implements Command {
+	private static final Logger logger = Logger.getLogger(CoffeeCommand.class.getName());
+
+	private final String apiUrl = "https://coffee.alexflipnote.dev/random.json";
+
+	@Override
+	public String name() {
+		return "coffee";
+	}
+
+	@Override
+	public List<String> aliases() {
+		return List.of("KAFFEEZEIT");
+	}
+
+	@Override
+	public HelpDoc help() {
+		//@formatter:off
+		return new HelpDoc.Builder(this)
+			.summary("Displays a coffee-related image.")
+			.detail("Images from coffee.alexflipnote.dev.")
+		.build();
+		//@formatter:on
+	}
+
+	@Override
+	public ChatActions onMessage(ChatCommand chatCommand, IBot bot) {
+		String url;
+		try {
+			url = getCoffee();
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, e, () -> "Problem getting coffee.");
+			return error("Error getting coffee: ", e, chatCommand);
+		}
+
+		//@formatter:off
+		return ChatActions.create(
+			new PostMessage(url).bypassFilters(true)
+		);
+		//@formatter:on
+	}
+
+	private String getCoffee() throws IOException {
+		try (Http http = HttpFactory.connect()) {
+			JsonNode node = http.get(apiUrl).getBodyAsJson().get("file");
+			if (node == null) {
+				throw new IOException("Unexpected JSON structure.");
+			}
+			return node.asText();
+		}
+	}
+}
