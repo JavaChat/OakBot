3a0c3fdd73dd2850fcac4b65b19886d75a89018c
Mike Angstadt <mike.angstadt@gmail.com>
2024-05-05 13:58:38 -0400

    Use text block

--- src/main/java/oakbot/CliArguments.java
+++ src/main/java/oakbot/CliArguments.java
@@ -42,42 +42,38 @@ public class CliArguments {
 	}
 
 	public String printHelp(String defaultContext) {
-		final var nl = System.lineSeparator();
-
-		//@formatter:off
-		return
-		"OakBot v" + Main.VERSION + nl +
-		"by Michael Angstadt" + nl +
-		Main.URL + nl +
-		nl +
-		"Arguments" + nl +
-		"================================================" + nl +
-		"--context=PATH" + nl +
-		"  The path to the Spring application context XML file that contains the bot's" + nl +
-		"  configuration settings and commands (defaults to \"" + defaultContext + "\")." + nl +
-		"  Note: Absolute paths must be prefixed with \"file:\"." + nl +
-		nl +
-		"--mock" + nl +
-		"  Runs the bot using a mock chat connection for testing purposes." + nl +
-		"  A text file will be created in the root of the project for each chat room the" + nl +
-		"  bot is configured to connect to. These files are used to \"send\" messages" + nl +
-		"  to the mock chat rooms. To send a message, type your message into the text" + nl +
-		"  file and save it." + nl +
-		"  Messages are entered one per line. Multi-line messages can be entered by" + nl +
-		"  ending each line with a backslash until you reach the last line. You should" + nl +
-		"  only append onto the end of the file; do not delete anything. These files are" + nl +
-		"  re-created every time the program runs." + nl +
-		"  All messages that are sent to the mock chat room are displayed in stdout (this" + nl +
-		"  includes your messages and the bot's responses)." + nl +
-		nl +
-		"--quiet" + nl +
-		"  If specified, the bot will not output a greeting message when it starts up." + nl +
-		nl +
-		"--version" + nl +
-		"  Prints the version of this program." + nl +
-		nl +
-		"--help" + nl +
-		"  Prints this help message.";
-		//@formatter:on
+		return """
+		OakBot v%s
+		by Michael Angstadt
+		%s
+		
+		Arguments
+		================================================
+		--context=PATH
+		  The path to the Spring application context XML file that contains the bot's
+		  configuration settings and commands (defaults to "%s").
+		  Note: Absolute paths must be prefixed with "file:".
+		
+		--mock
+		  Runs the bot using a mock chat connection for testing purposes.
+		  A text file will be created in the root of the project for each chat room the
+		  bot is configured to connect to. These files are used to "send" messages
+		  to the mock chat rooms. To send a message, type your message into the text
+		  file and save it.
+		  Messages are entered one per line. Multi-line messages can be entered by
+		  ending each line with a backslash until you reach the last line. You should
+		  only append onto the end of the file; do not delete anything. These files are
+		  re-created every time the program runs.
+		  All messages that are sent to the mock chat room are displayed in stdout (this
+		  includes your messages and the bot's responses).
+		
+		--quiet
+		  If specified, the bot will not output a greeting message when it starts up.
+		
+		--version
+		  Prints the version of this program.
+		
+		--help
+		  Prints this help message.""".formatted(Main.VERSION, Main.URL, defaultContext);
 	}
 }
